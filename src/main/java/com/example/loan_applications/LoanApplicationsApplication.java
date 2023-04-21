89301934d15c1e98b583ca25af8aa08732ec1dac
Evdokimov <danila.evdokimov.444@mail.ru>
2023-04-21 04:44:57 +0300

    Initial commit

---
+++ src/main/java/com/example/loan_applications/LoanApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.loan_applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplicationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loan_applications/LoanApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loan_applications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
