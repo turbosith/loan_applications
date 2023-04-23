c0e4ee29eb37c51785a62383b56ac574f1a514e1
Evdokimov <danila.evdokimov.444@mail.ru>
2023-04-23 16:24:03 +0300

    add Tariff and Tariff TariffDTO

--- src/main/java/com/example/loan_applications/model/Tariff.java
+++ src/main/java/com/example/loan_applications/model/Tariff.java
@@ -1,4 +1,14 @@
 package com.example.loan_applications.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tariff {
+    private long id;
+    private String type;
+    private String interestRate;
 }
