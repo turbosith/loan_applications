b59a269a6b47e0c58588c1dc961c815652931f5c
Evdokimov <danila.evdokimov.444@mail.ru>
2023-04-23 14:10:45 +0300

    project structure

---
+++ src/main/java/com/example/loan_applications/model/Tariff.java
@@ -0,0 +1,4 @@
+package com.example.loan_applications.model;
+
+public class Tariff {
+}
---
+++ src/main/java/com/example/loan_applications/repository/TariffRepository.java
@@ -0,0 +1,13 @@
+package com.example.loan_applications.repository;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+@Repository
+@AllArgsConstructor
+public class TariffRepository {
+    
+
+}
