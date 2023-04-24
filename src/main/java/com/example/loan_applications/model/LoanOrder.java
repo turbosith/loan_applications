7840b060151b89a7f5fbaf0667c5a12b45ece252
Evdokimov <danila.evdokimov.444@mail.ru>
2023-04-25 00:00:13 +0300

    add LoanOrder

--- src/main/java/com/example/loan_applications/model/LoanOrder.java
+++ src/main/java/com/example/loan_applications/model/LoanOrder.java
@@ -1,4 +1,16 @@
 package com.example.loan_applications.model;
 
+import com.example.loan_applications.constants.StatusEnum;
+import lombok.Data;
+
+@Data
 public class LoanOrder {
+    private long id;
+    private String orderId;
+    private long userId;
+    private long tariffId;
+    private double creditRating;
+    private StatusEnum status;
+
+
 }
