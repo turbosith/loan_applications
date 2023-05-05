3764215b75c015261a941f6768789d5b90a5590a
Evdokimov <danila.evdokimov.444@mail.ru>
2023-05-05 23:55:13 +0300

    add test methods

--- src/test/java/com/example/loan_applications/TariffIntegrationTest.java
+++ src/test/java/com/example/loan_applications/TariffIntegrationTest.java
@@ -1,4 +1,27 @@
 package com.example.loan_applications;
 
+import com.example.loan_applications.service.TariffService;
+import com.example.loan_applications.service.impl.TariffServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TariffIntegrationTest {
+
+    @Autowired
+    private TariffService tariffService;
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void getTariffsTest(){
+
+    }
+    @Test
+    public void addTariffTest(){
+
+    }
 }
